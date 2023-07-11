11e5a2fe62c9def363c0b2ea8fb478f6f7a42d84
Song <scw387@naver.com>
2023-07-11 14:30:03 +0900

    테이블 유저데이터를 사이트에 출력

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
-  return <div></div>;
-};
+function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  //async는 만약 함수 function 앞에 붙임
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:8080/users");
+    setUsers(result.data);
+  };
+  return (
+    <div className="container">
+      <table className="table border text-center shadow my-4">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">이름</th>
+            <th scope="col">닉네임</th>
+            <th scope="col">EMAIL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
 export default Home;
