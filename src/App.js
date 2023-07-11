62f5cd4036773cc2cdd57159e7164528748664d3
Song <scw387@naver.com>
2023-07-11 15:34:24 +0900

    라우터 라이브러리 설치 &가입하기 화면

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.css";
 import Home from "./pages/Home";
+import AddUser from "./users/AddUser";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
