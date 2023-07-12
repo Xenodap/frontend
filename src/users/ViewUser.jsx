8a371b723fb6055cb406fb3ee558df9ad5b546fd
Song <scw387@naver.com>
2023-07-12 14:32:50 +0900

    완성

--- src/users/ViewUser.jsx
+++ src/users/ViewUser.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const ViewUser = () => {
+  //요청주소의 id값을 받음
+  const { id } = useParams();
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  const loadUsers = async () => {
+    const result = await axios.get(`http://localhost:8080/users/${id}`);
+    setUser(result.data);
+  };
+
+  const { name, username, email } = user;
+
   return (
     <div className="container">
       <div className="row">
@@ -10,16 +31,16 @@ const ViewUser = () => {
 
           <div className="card">
             <div className="card-header">
-              유저ID :
+              유저ID :{id}
               <ul className="list-group list-group-flush">
                 <li className="list-group-item">
-                  <b>이름 : </b>
+                  <b>이름 :{name} </b>
                 </li>
                 <li className="list-group-item">
-                  <b>유저네임 : </b>
+                  <b>유저네임 :{username} </b>
                 </li>
                 <li className="list-group-item">
-                  <b>이메일 : </b>
+                  <b>이메일 :{email} </b>
                 </li>
               </ul>
             </div>
